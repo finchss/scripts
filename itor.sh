07fc51a774df3f73ddab5b5d3831252f049c4263
root <root@debian>
2019-03-23 19:26:09 -0400

    Redirect all output to /dev/null

--- itor.sh
+++ itor.sh
@@ -1,4 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
+killall -9 tor
 
 if [ ! -f /usr/bin/tor ]
 then
@@ -11,19 +12,26 @@ then
 	apt-get update
 	apt-get install -y  tor-geoipdb
 fi
-
+echo -n "Starting $1 tor servers, please wait "
 for ((i=1;i<=$1;i++))
 do
+	echo -n '*'
+	if [ ! -d /var/lib/tor$i ] 
+	then
+	    mkdir /var/lib/tor$i
+	fi
 
+    
+	if [ ! -d /var/run/tor$i ] 
+	then 
+	    mkdir /var/run/tor$i
+	fi
 
-	mkdir /var/lib/tor$i
 	chown debian-tor.debian-tor /var/lib/tor$i
-
-	mkdir /var/run/tor$i
 	chown debian-tor.debian-tor /var/run/tor$i
-	chmod 2750 /var/run/tor$i/
-	#mkdir /var/log/tor$i
-	#chown debian-tor.adm /var/log/tor$i
+
+	chmod 0700 /var/lib/tor$i/
+	chmod 2755 /var/run/tor$i/
 
 
 	cat > /usr/share/tor/tor-service-defaults-torrc$i  <<EOF
@@ -47,8 +55,11 @@ EOF
 	echo 'EntryNodes {RO}' >> /etc/tor/torrc$i
 	echo "Log debug file /dev/null " >>  /etc/tor/torrc$i
 	echo "Log notice file /dev/null" >> /etc/tor/torrc$i
-	echo "Log debug file /dev/null" >>  /etc/tor/torrc$i
 
-	/usr/bin/tor --defaults-torrc /usr/share/tor/tor-service-defaults-torrc$i -f /etc/tor/torrc$i --RunAsDaemon 1
+	/usr/bin/tor --defaults-torrc /usr/share/tor/tor-service-defaults-torrc$i -f /etc/tor/torrc$i --RunAsDaemon 1 >/dev/null 2>/dev/null
+	echo -n -e '\b'.
 done
+echo " Done !"
+count=`ps axf|grep tor |grep -v grep |wc -l`
+echo $count tor servers running 
 
