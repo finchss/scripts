db879289c730048421693bdd5bffcf47aa47671f
steve <steve@debian.tentb.com>
2019-06-24 20:57:32 +0300

    added a small handler for ssh-links

---
+++ ssh.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+<?php
+
+    $i=$argv[1];
+    $i=str_replace("ssh://","",$i);
+    $a=explode("@",$i);
+    //var_dump($a);
+    $up=$a[0];
+    for($i=1;$i<count($a)-1;$i++) {
+	if($a[$i]=="")
+	    $up.="@";
+	else $up.="@".$a[$i];
+    }
+    $ipp=str_replace("/","",str_replace("\n","",$a[count($a)-1]));
+    echo $up."\n";
+    echo $ipp."\n";
+    list($ip,$port)=explode(":",$ipp);
+    list($user,$pass)=explode(":",$up);
+    echo $ip."\n";
+    echo $port."\n";
+    echo $user."\n";
+    echo $pass."\n";
+    $x=mt_rand(0,9999999);
+    file_put_contents("/tmp/cmd$x.sh","#!/bin/bash\n. torsocks on\nsshpass -p \"$pass\" ssh -v $user@$ip -p $port \n");
+    chmod("/tmp/cmd$x.sh",0755);
+    system("terminator -e /tmp/cmd$x.sh");
+    system("rm -f /tmp/cmd$x.sh");
+?>
