69d24f2e6c1572ed9eec9ccbafb124d62056a3f3
steve <steve@debian.tentb.com>
2019-09-23 12:57:20 +0300

    Nightly commit

--- parse_ssh_log.php
+++ parse_ssh_log.php
@@ -8,16 +8,13 @@
 	
 	while(!feof($fp)) {
 		$s=fgets($fp);
-		//echo $s."\n";
 		if(!strstr($s,"password")) continue;
 		$x=explode(" from ",$s);
 		$y=explode(" ",$x[0]);
 		$user=$y[count($y)-1];
-		//echo $x[1]."\n";
 		list($none,$x)=explode("password: ",$x[1]);
 		$x=substr($x,0,strlen($x)-2);
 		echo"$user:$x\n";
-	//	echo $user."\n";
 	}
 
 ?>
