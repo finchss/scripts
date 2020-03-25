9c14fdb229920aea9867fe718131cdc07610f0bd
root <root@debian>
2020-03-25 17:39:30 +0800

    added local interface tracking for he.net

--- HeNetDnsUpdate.sh
+++ HeNetDnsUpdate.sh
@@ -1,38 +1,66 @@
 #!/bin/bash
 
 
-if [ $# -ne 2 ]
+ns="ns2.he.net"
+
+if  [ $# -ne 2 ] && [ $# -ne 3 ] 
 then
 	echo "Usage:"
 	echo
-	echo "./HeNetDnsUpdate.sh <hostname> <password>"
+	echo -e "\t./HeNetDnsUpdate.sh <hostname> <password>"
+	echo 
+	echo -e "\t./HeNetDnsUpdate.sh <interface> <hostname> <password>"
 	echo
 	exit 
 fi
 
-if (  which host >/dev/null 2 >&1 ) 
-then 
-	echo '"host" is not installed'
-	echo -e "On debian you can use \"apt-get install bind9-host\" to install it"
-	exit
-fi
+case $# in 
+	2)
+		if (  which host >/dev/null 2 >&1 ) 
+		then 
+			echo '"host" is not installed'
+			echo -e "On debian you can use \"apt-get install bind9-host\" to install it"
+			exit
+		fi
 
 
 
-if (  which curl >/dev/null 2 >&1 ) 
-then 
-	echo '"curl" is not installed'
-	echo -e "On debian you can use \"apt-get install curl\" to install it"
-	exit
-fi
+		if (  which curl >/dev/null 2 >&1 ) 
+		then 
+			echo '"curl" is not installed'
+			echo -e "On debian you can use \"apt-get install curl\" to install it"
+			exit
+		fi
 
-ip=`curl -4 -k https://api.ipify.org 2>/dev/null `
-lip=`host -4 $1 ns1.he.net  | grep 'has address' |awk '{print $4}'`
+		ip=`curl -4 -k https://api.ipify.org 2>/dev/null `
+		lip=`host -4 $1 {$ns}  | grep 'has address' |awk '{print $4}'`
 
-if [  "$ip" != "$lip" ] 
-then 
-    echo Updating $1
-    curl -4 -k "https://$1:$2@dyn.dns.he.net/nic/update?hostname=$1"
-else
-    echo "Ip hasn't changed ($ip)"
-fi
+		if [  "$ip" != "$lip" ] 
+		then 
+			echo Updating $1
+			curl -4 -k "https://$1:$2@dyn.dns.he.net/nic/update?hostname=$1"
+		else
+			echo "Ip hasn't changed ($ip)"
+		fi
+	;;
+	3)
+		iface=$1
+		hname=$2
+		pass=$3
+		if ( cat /proc/net/dev |grep "${iface}:" >/dev/null 2>&1) 
+		then
+			iface_ip=`ip addr show dev ${iface} |grep inet |grep brd |awk '{print $2}' |cut -f 1 -d /`
+			iip=`host -4 ${hname} ${ns} | grep 'has address' |awk '{print $4}'`
+			if [ "${iip}" != "${iface_ip}" ]
+			then
+				echo "Host $hname resolved to $iip, updating to $iface_ip"
+				curl -4 -k "https://${hname}:${pass}@dyn.dns.he.net/nic/update?hostname=${hname}&myip=${iface_ip}"
+			fi
+		else
+			echo "No such interface "
+			exit 1
+		fi
+		
+	;;
+esac
+
