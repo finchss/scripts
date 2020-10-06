27c5258d71e044631478dadc051bdceb6a54d257
steve <steve@steves-MacBook-Pro.local>
2020-10-06 12:26:40 +0800

    Added path check, ifconfig problems on macos

--- HeNetDnsUpdate.sh
+++ HeNetDnsUpdate.sh
@@ -3,6 +3,13 @@
 
 ns="ns1.he.net"
 
+
+if [ -z ${PATH+x} ];then
+	PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+else
+	PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+fi
+
 if  [ $# -ne 2 ] && [ $# -ne 3 ] 
 then
 	echo "Usage:"
@@ -32,13 +39,13 @@ case $# in
 			exit
 		fi
 
-		ip=`curl -4 -k https://api.ipify.org 2>/dev/null `
+		ip=`curl -s -4 -k https://api.ipify.org 2>/dev/null `
 		lip=`host -4 $1 {$ns}  | grep 'has address' |awk '{print $4}'`
 
 		if [  "$ip" != "$lip" ] 
 		then 
 			echo Updating $1
-			curl -4 -k "https://$1:$2@dyn.dns.he.net/nic/update?hostname=$1"
+			curl -s -4 -k "https://$1:$2@dyn.dns.he.net/nic/update?hostname=$1"
 		else
 			echo "Ip hasn't changed ($ip)"
 		fi
@@ -56,10 +63,14 @@ case $# in
 		
 		if [ $OSTYPE == "linux-gnu" ] ;then
 			iface_ip=`ip addr show dev ${iface} |grep inet |grep brd |awk '{print $2}' |cut -f 1 -d /`
-		fi
-		
-		if [[ $OSTYPE == darwin* ]]; then
-			iface_ip=`ifconfig ${iface} |grep -E 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -o |cut -f 2 -d " "`
+		else 
+			if [[ $OSTYPE == darwin* ]]; then
+				iface_ip=`ifconfig ${iface} |grep -E 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' -o |cut -f 2 -d " "`
+			else
+				echo "Unknown os or vsar not set"
+				exit 1
+				
+			fi
 		fi
 			
 		
@@ -67,7 +78,7 @@ case $# in
 		if [ "${iip}" != "${iface_ip}" ] && [ ! -z ${iip} ]
 		then
 			echo "Host $hname resolved to $iip, updating to $iface_ip"
-			curl -4 -k "https://${hname}:${pass}@dyn.dns.he.net/nic/update?hostname=${hname}&myip=${iface_ip}"
+			curl -s -4 -k "https://${hname}:${pass}@dyn.dns.he.net/nic/update?hostname=${hname}&myip=${iface_ip}"
 			echo 
 		else
 			echo "Ip is the same: ${iip}"
